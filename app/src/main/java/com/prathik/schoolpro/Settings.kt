42856b7eff8d894a4da0fbcb31011dbbf4b77d2a
MUCE903 <prathikpremanpp@gmail.com>
2020-02-20 13:20:00 +0400

    google authenticator added

---
+++ app/src/main/java/com/prathik/schoolpro/Settings.kt
@@ -0,0 +1,60 @@
+package com.prathik.schoolpro
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import com.prathik.schoolpro.util.BiometricUtils
+import com.prathik.schoolpro.util.PreferenceManager
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class Settings : AppCompatActivity() {
+
+    lateinit var obj: BiometricUtils
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        actionBar?.title="Settings"
+
+        isFingerPrintAvailable()
+        setTwoStepAuth()
+
+    }
+
+    private fun setTwoStepAuth() {
+        twoStepBtn.setOnClickListener {
+            startActivity(Intent(this,TwoWayAuthentication::class.java))
+        }
+
+    }
+
+
+    private fun isFingerPrintAvailable() {
+        obj= BiometricUtils()
+        if(!(obj.isHardwareSupported(this))){  // no hardware
+            btn_fingerPrint.visibility=View.GONE
+        }
+        else {
+            fingerPrintSwitch.isChecked = PreferenceManager.getBoolValue(this,PreferenceManager.IF_FINGER_PRINT_ENABLED)
+
+            fingerPrintSwitch.setOnCheckedChangeListener { _, isChecked->
+
+                PreferenceManager.setBoolValue(this,PreferenceManager.IF_FINGER_PRINT_ENABLED,isChecked)
+
+                if(isChecked && !(obj.isFingerprintAvailable(this))){
+                    Toast.makeText(this,"Please Add atleast one Fingerprint from phone Settings to continue",Toast.LENGTH_LONG).show()
+                    PreferenceManager.setBoolValue(this,PreferenceManager.IF_FINGER_PRINT_ENABLED,false)
+                    fingerPrintSwitch.isChecked=false
+                }
+
+
+            }
+
+        }
+
+    }
+}
